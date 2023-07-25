8c8d8bb1076436b0f48aa6d5e0649024091b7a39
steffancarrington <steffan@pennyblack.io>
2023-07-25 15:39:22 +0100

    MISC - y co-ordinates of text field schema updated to accommodate new generator logic

--- src/helper.ts
+++ src/helper.ts
@@ -103,7 +103,10 @@ export const getTemplate = () => {
       {
         name: {
           type: "text",
-          position: { x: 25.06, y: 26.35 },
+          position: {
+            x: 25.06,
+            y: 24.35
+          },
           width: 77.77,
           height: 18.7,
           fontSize: 36,
@@ -111,41 +114,59 @@ export const getTemplate = () => {
         },
         photo: {
           type: "image",
-          position: { x: 24.99, y: 65.61 },
+          position: {
+            x: 24.99,
+            y: 65.61
+          },
           width: 60.66,
-          height: 93.78,
+          height: 93.78
         },
         age: {
           type: "text",
-          position: { x: 36, y: 179.46 },
+          position: {
+            x: 35.5,
+            y: 178.46
+          },
           width: 43.38,
           height: 6.12,
           fontSize: 12,
         },
         sex: {
           type: "text",
-          position: { x: 36, y: 186.23 },
+          position: {
+            x: 35.5,
+            y: 185.23
+          },
           width: 43.38,
           height: 6.12,
           fontSize: 12,
         },
         weight: {
           type: "text",
-          position: { x: 40, y: 192.99 },
+          position: {
+            x: 41.05,
+            y: 192.26
+          },
           width: 43.38,
           height: 6.12,
           fontSize: 12,
         },
         breed: {
           type: "text",
-          position: { x: 40, y: 199.09 },
+          position: {
+            x: 39,
+            y: 199.09
+          },
           width: 43.38,
           height: 6.12,
           fontSize: 12,
         },
         owner: {
           type: "qrcode",
-          position: { x: 115.09, y: 204.43 },
+          position: {
+            x: 115.09,
+            y: 204.43
+          },
           width: 26.53,
           height: 26.53,
         },
